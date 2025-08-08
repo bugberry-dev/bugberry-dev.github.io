3da82ab011c06deee9ffab5996099a6290f7fb97
Henrik <henrik.haapamaeki@gmail.com>
2025-08-08 10:22:19 +0200

    Initial commit

---
+++ assets/js/sendmail.js
@@ -0,0 +1,12 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    const subject = encodeURIComponent(document.getElementById('subject').value);
+    const message = encodeURIComponent(document.getElementById('message').value);;
+
+    // Compose mailto link with subject and body
+    const mailtoLink = `mailto:henrik.haapamaeki@gmail.com?subject=${subject}&body=${message}`;
+
+    // Open user's mail client
+    window.location.href = mailtoLink;
+});
